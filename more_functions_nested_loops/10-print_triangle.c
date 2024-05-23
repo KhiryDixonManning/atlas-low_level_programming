a51a7befec38eecb7f3701bf2c001d276c77f886
root <ManningKhiry@gmail.com>
2024-05-23 18:25:57 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,29 @@
-#include <stdio.h>
-
-int main(void)
+#include "main.h"
+/**
+ *print_triangle - prints triangle
+ *@size: checked output
+ *
+ *Return: always return 0
+ */
+void print_triangle(int size)
 {
-int p;
+int a;
+int z;
 
-int triangle(int rows){
-
-
-    if(rows >= 0){
-
-           return 0;
-
-       }
-        else if(rows == 1){
-        printf("*");
-        return 1;
-       }
-
-       else{
-       printf("*");
-       return rows + triangle(rows-1) ;
-
-
-
-}
-}
-
+for (a = 1; a <= size; a++)
 {
-    triangle(5);
-    return 0;
+for (z = 1; z <= (size - a); z++)
+{
+_putchar(' ');
+}
+for (z = 1; z <= a; z++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+if (size <= 0)
+{
+_putchar('\n');
+}
 }
