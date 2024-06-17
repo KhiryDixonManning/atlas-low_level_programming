3cc1eb1d1d513da00bf17312da83218edf4eb773
root <ManningKhiry@gmail.com>
2024-06-17 17:04:44 +0000

    number 6

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,32 +1,46 @@
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
- *str_concat - concatenate two strings
- *@s1: checked output
- *@s2: checked output
+ * str_concat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
  *
- *Return: always return 0
+ * Return: pointer to newly allocated memory containing concatenated strings
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *result;
-	int len1, len2;
+	int len1 = 0, len2 = 0;
+	int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	/* Calculate length of s1 */
+	while (s1[len1] != '\0')
+		len1++;
 
-	result = malloc(len1 + len2 + 1);
+	/* Calculate length of s2 */
+	while (s2[len2] != '\0')
+		len2++;
+
+	result = malloc(sizeof(char) * (len1 + len2 + 1));
+
 	if (result == NULL)
 		return (NULL);
 
-	strcpy(result, s1);
-	strcat(result, s2);
+	/* Copy s1 to result */
+	for (i = 0; s1[i] != '\0'; i++)
+		result[i] = s1[i];
+
+	/* Concatenate s2 to result */
+	for (j = 0; s2[j] != '\0'; j++)
+		result[i + j] = s2[j];
+
+	result[i + j] = '\0';
 
 	return (result);
 }
+
