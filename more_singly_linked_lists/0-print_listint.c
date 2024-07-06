dcd8d518c7bb9cf56f19c4c82561fa35a8619fb4
root <ManningKhiry.com>
2024-07-06 01:05:06 +0000

    ahhh

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,21 @@
 #include "lists.h"
+
 /**
- *print_listint - print elements of the list
- *@h: checked output
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the head of the list
  *
- *Return: always return 0
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-	return (count);
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
+    return (count);
 }
+
