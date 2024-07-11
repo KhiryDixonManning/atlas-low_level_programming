edc229471cbd281ccc1db8eaf8e30bc4351f4afe
root <ManningKhiry@gmail.com>
2024-07-11 16:40:47 +0000

    rest after 6 to see if 7 will work

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+/**
+ *insert_dnodeint_at_index - inserts a node at index
+ *@h: checked output
+ *@idx: checked output
+ *@n: checked output
+ *
+ *Return: always return 0
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *current;
+	unsigned int i;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+
+	current = *h;
+	for (i = 0; current != NULL && i < idx - 1; i++)
+		current = current->next;
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+	new_node->prev = current;
+
+	return (new_node);
+}
